9cbf4fd39926fe8f7a4c623f4f667666e4c370f8
martinaliporata <martinaliporata@gmail.com>
2024-06-10 15:08:13 +0200

    implementing vue and sass installation

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+  import AppHeader from './components/AppHeader.vue';
+  import AppMain from './components/AppMain.vue';
+  import AppFooter from './components/AppFooter.vue';
+  export default {
+    components: {
+      AppHeader,
+      AppMain,
+      AppFooter,
+    }
+  }
+</script>
+
+<template>
+  <AppHeader/>
+  <AppMain/>
+  <AppFooter/>
+</template>
+
+<style scoped>
+
+</style>
